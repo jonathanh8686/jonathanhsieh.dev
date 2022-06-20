9a0578a1bb179f538beb38df1f80874b57d2765c
Jonathan Hsieh <jonathanh1386@gmail.com>
2022-06-20 06:01:15 -0400

    this has gotta be it

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(helmet());
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build/')))
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm/')))
 
-app.listen(80, () => {
-    console.log("Server running on port 80")
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log("Server running on port " + port)
 })
