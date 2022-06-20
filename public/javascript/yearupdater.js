8e8e311d51dc9d50a2704bb41c04d770ea97d87e
Jonathan Hsieh <jonathanh1386@gmail.com>
2022-06-20 05:41:17 -0400

    i can't remember

---
+++ public/javascript/yearupdater.js
@@ -0,0 +1,11 @@
+var _initial = '2020-09-09T00:00:00.000Z';
+var fromTime = new Date(_initial);
+
+var totalSeconds = 115100000
+
+const interval = setInterval(() => {
+    var toTime = new Date();
+    var differenceTravel = toTime.getTime() - fromTime.getTime();
+    var seconds = differenceTravel / 1000;
+    document.getElementById("year-dec").innerText = (4*seconds/totalSeconds).toString().substring(0, 10n);
+}, 1)
