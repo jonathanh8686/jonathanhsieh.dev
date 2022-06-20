5e27e664575ba5e615d6b33bbe110d83523db589
Jonathan Hsieh <jonathanh1386@gmail.com>
2022-06-20 05:41:24 -0400

    can't remember

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+var helmet = require('helmet');
 
 app.use(express.static(__dirname + '/public'))
+app.use(helmet());
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build/')))
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm/')))
 
