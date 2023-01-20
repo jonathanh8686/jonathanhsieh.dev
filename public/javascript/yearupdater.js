bb2a7b9347cebd1bf3b0dfc53034311a1ae2ec2d
Jonathan Hsieh <jonathanh1386@gmail.com>
2023-01-20 17:23:18 -0500

    i don't like the description

--- public/javascript/yearupdater.js
+++ public/javascript/yearupdater.js
@@ -1,11 +1,12 @@
-var _initial = '2020-09-09T00:00:00.000Z';
+var _initial = '2002-07-31T00:00:00.000Z';
 var fromTime = new Date(_initial);
-
-var totalSeconds = 115100000
 
 const interval = setInterval(() => {
     var toTime = new Date();
     var differenceTravel = toTime.getTime() - fromTime.getTime();
     var seconds = differenceTravel / 1000;
-    document.getElementById("year-dec").innerText = (4*seconds/totalSeconds).toString().substring(0, 10);
+    var years = seconds / 31536000
+    // var str_seconds = String.format("%-5s", seconds).replace(' ', '0')
+
+    document.getElementById("year-dec").innerText = years.toString().substring(0, 11);
 }, 1)
